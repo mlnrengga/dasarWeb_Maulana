08f3aadc3b82c2f4d4d61093a8bcf5bc8be8fcf1
mlnrengga <mlnrengga@gmail.com>
2024-09-18 12:37:02 +0700

    Prak3_soal3.2

--- Jobsheet 4/operator.php
+++ Jobsheet 4/operator.php
@@ -14,5 +14,19 @@ echo "hasil dari $a - $b adalah $hasilKurang <br>";
 echo "hasil dari $a x $b adalah $hasilKali <br>";
 echo "hasil dari $a : $b adalah $hasilBagi <br>";
 echo "sisa bagi dari $a dan $b adalah $sisaBagi <br>";
-echo "hasil dari $a pangkat $b adalah $pangkat";
+echo "hasil dari $a pangkat $b adalah $pangkat <br>";
+
+$hasilSama = $a == $b;
+$hasilTidakSama = $a != $b;
+$hasilLebihKecil = $a < $b;
+$hasilLebihBesar = $a > $b;
+$hasilLebihKecilSama = $a <= $b;
+$hasilLebihBesarSama = $a >= $b;
+
+echo "apakah $a dan $b memiliki nilai yang sama (+) ? " . var_export($hasilSama, true) . "<br>";
+echo "apakah $a dan $b memiliki nilai yang tidak sama (!=) ? " . var_export($hasilTidakSama, true) . "<br>";
+echo "apakah $a lebih kecil dari pada (<) $b? " . var_export($hasilLebihKecil, true) . "<br>";
+echo "apakah $a lebih besar dari pada (>) $b? " . var_export($hasilLebihBesar, true) . "<br>";
+echo "apakah $a lebih kecil atau sama dengan (=>)  $b? " . var_export($hasilLebihKecilSama, true) . "<br>";
+echo "apakah $a lebih besar atau sama dengan (>=) $b? " . var_export($hasilLebihBesarSama, true) . "<br>";
 ?>
