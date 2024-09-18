012832d6a374e0b5539a605e0afaca3bcf9bf656
mlnrengga <mlnrengga@gmail.com>
2024-09-18 11:42:17 +0700

    Prak3_soal3.1

---
+++ Jobsheet 4/operator.php
@@ -0,0 +1,18 @@
+<?php 
+$a = 10;
+$b = 5;
+
+$hasilTambah = $a + $b;
+$hasilKurang = $a - $b;
+$hasilKali = $a * $b;
+$hasilBagi = $a / $b;
+$sisaBagi = $a % $b;
+$pangkat = $a ** $b;
+
+echo "hasil dari $a + $b adalah $hasilTambah <br>";
+echo "hasil dari $a - $b adalah $hasilKurang <br>";
+echo "hasil dari $a x $b adalah $hasilKali <br>";
+echo "hasil dari $a : $b adalah $hasilBagi <br>";
+echo "sisa bagi dari $a dan $b adalah $sisaBagi <br>";
+echo "hasil dari $a pangkat $b adalah $pangkat";
+?>
