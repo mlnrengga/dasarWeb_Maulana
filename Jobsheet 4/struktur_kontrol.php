3bb5f6cdb1de7482df0787603473608af262a209
mlnrengga <mlnrengga@gmail.com>
2024-09-19 19:23:16 +0700

    Prak4_soal4.3

--- Jobsheet 4/struktur_kontrol.php
+++ Jobsheet 4/struktur_kontrol.php
@@ -1,27 +1,42 @@
 <?php
-$nilaiNumerik = 92;
+// $nilaiNumerik = 92;
 
-if ($nilaiNumerik >= 90 && $nilaiNumerik <= 100) {
-    echo "Nilai huruf: A";
-} elseif ($nilaiNumerik >= 80 && $nilaiNumerik < 90) {
-    echo "Nilai huruf: B";
-} elseif ($nilaiNumerik >= 70 && $nilaiNumerik < 80) {
-    echo "Nilai huruf: C";
-} elseif ($nilaiNumerik < 70) {
-    echo "Nilai huruf: D";
+// if ($nilaiNumerik >= 90 && $nilaiNumerik <= 100) {
+//     echo "Nilai huruf: A";
+// } elseif ($nilaiNumerik >= 80 && $nilaiNumerik < 90) {
+//     echo "Nilai huruf: B";
+// } elseif ($nilaiNumerik >= 70 && $nilaiNumerik < 80) {
+//     echo "Nilai huruf: C";
+// } elseif ($nilaiNumerik < 70) {
+//     echo "Nilai huruf: D";
+// }
+// echo ("<br>");
+
+// $jarakSaatIni = 0;
+// $jarakTarget = 500;
+// $peningkatanHarian = 30;
+// $hari = 0;
+
+// while ($jarakSaatIni < $jarakTarget) {
+//     $jarakSaatIni += $peningkatanHarian;
+//     $hari++;
+//     echo "<br> Hari ke-$hari jarak yang sudah di tempuh oleh atlet adalah $jarakSaatIni kilometer";
+// }
+
+// echo "<br>Atlet tersebut memerlukan $hari hari untuk mencapai jarak 500 kilometer.";
+
+$jumlahLahan = 10;
+$tanamanPerLahan = 5;
+$buahPerTanaman = 10;
+$jumlahBuah = 0;
+
+for ($i = 1; $i <= $jumlahLahan; $i++) {
+    $jumlahBuah += ($tanamanPerLahan * $buahPerTanaman);
 }
-echo ("<br>");
 
-$jarakSaatIni = 0;
-$jarakTarget = 500;
-$peningkatanHarian = 30;
-$hari = 0;
 
-while ($jarakSaatIni < $jarakTarget) {
-    $jarakSaatIni += $peningkatanHarian;
-    $hari++;
-    echo "<br> Hari ke-$hari jarak yang sudah di tempuh oleh atlet adalah $jarakSaatIni kilometer";
-}
-
-echo "<br>Atlet tersebut memerlukan $hari hari untuk mencapai jarak 500 kilometer.";
+echo "Jumlah lahan: $jumlahLahan <br>";
+echo "Jumlah tanaman per lahan: $tanamanPerLahan<br>";
+echo "Jumlah buah per tanaman: $buahPerTanaman<br>";
+echo "Jumlah buah yang akan dipanen adalah: $jumlahBuah";
 ?>
