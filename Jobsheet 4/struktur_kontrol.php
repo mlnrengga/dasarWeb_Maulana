8ea504e58360520756fc7986191ea7457091e978
mlnrengga <mlnrengga@gmail.com>
2024-09-19 20:06:12 +0700

    Prak4_soal4,5

--- Jobsheet 4/struktur_kontrol.php
+++ Jobsheet 4/struktur_kontrol.php
@@ -59,4 +59,16 @@ foreach ($skorUjian as $skor) {
 }
 
 echo "Total skor ujian adalah: $totalSkor";
+echo "<br>";
+
+$nilaiSiswa = [85, 92, 58, 64, 90, 55, 88, 79, 70, 96];
+
+foreach ($nilaiSiswa as $nilai) {
+    if ($nilai < 60) {
+        echo "Nilai: $nilai (Tidak lulus) <br>";
+        continue;
+    }
+
+    echo "Nilai: $nilai (Lulus) <br>";
+}
 ?>
