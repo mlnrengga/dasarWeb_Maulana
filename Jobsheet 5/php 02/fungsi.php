81a13f2a126940d401a1bf7e3cae5dcf303ef74c
mlnrengga <mlnrengga@gmail.com>
2024-09-24 09:38:56 +0700

    Prak1_soal5

--- Jobsheet 5/php 02/fungsi.php
+++ Jobsheet 5/php 02/fungsi.php
@@ -32,6 +32,16 @@ function hitungUmur($thn_lahir, $thn_sekarang) {
     $umur = $thn_sekarang - $thn_lahir;
     return $umur;
 }
+function perkenalan($nama, $salam="Assalamualaikum") {
+    echo $salam.", ";
+    echo "Perkenalkan, nama saya ".$nama."<br/>";
 
-echo "Umur saya adalah ". hitungUmur(2004, 2024) ." tahun"; // isi sesuai dengan tahun lahir kalian
+    //memanggil fungsi lain
+    echo "Saya berusia ". hitungUmur(1988, 2023) ." tahun<br/>"; 
+    echo "Senang berkenalan dengan Anda<br/>";
+}
+
+//memanggil fungsi perkenalan
+perkenalan("Elok");
+
 ?>
