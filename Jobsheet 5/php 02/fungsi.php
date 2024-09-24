4bf56a721a11f5582c943dc03a12de1f786d5fc7
mlnrengga <mlnrengga@gmail.com>
2024-09-24 09:07:39 +0700

    Prak1_soal4

--- Jobsheet 5/php 02/fungsi.php
+++ Jobsheet 5/php 02/fungsi.php
@@ -11,20 +11,27 @@
 // perkenalan();
 
 //membuat fungsi
-function perkenalan($nama, $salam="Assalamualaikum") {
-    echo $salam.", ";
-    echo "Perkenalkan, nama saya ".$nama."<br/>";
-    echo "Senang berkenalan dengan Anda<br/>";
+// function perkenalan($nama, $salam="Assalamualaikum") {
+//     echo $salam.", ";
+//     echo "Perkenalkan, nama saya ".$nama."<br/>";
+//     echo "Senang berkenalan dengan Anda<br/>";
+// }
+
+// //memanggil fungsi yang sudah dibuat
+// perkenalan("Hamdana", "Hallo");
+
+// echo "<hr>";
+
+// $saya = "Elok";
+// $ucapanSalam = "Selamat pagi";
+
+// //memanggil lagi
+// perkenalan($saya);
+//membuat fungsi
+function hitungUmur($thn_lahir, $thn_sekarang) {
+    $umur = $thn_sekarang - $thn_lahir;
+    return $umur;
 }
 
-//memanggil fungsi yang sudah dibuat
-perkenalan("Hamdana", "Hallo");
-
-echo "<hr>";
-
-$saya = "Elok";
-$ucapanSalam = "Selamat pagi";
-
-//memanggil lagi
-perkenalan($saya);
+echo "Umur saya adalah ". hitungUmur(1988, 2023) ." tahun"; // isi sesuai dengan tahun lahir kalian
 ?>
