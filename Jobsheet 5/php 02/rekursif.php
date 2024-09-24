6bed12250c4898d7da0c9ed9e2485d08e16a12e0
mlnrengga <mlnrengga@gmail.com>
2024-09-24 10:25:55 +0700

    Prak1_soal6

---
+++ Jobsheet 5/php 02/rekursif.php
@@ -0,0 +1,19 @@
+<?php 
+// function tampilkanHaloDunia() {
+//     echo "Halo dunia! <br>";
+    
+//     tampilkanHaloDunia();
+// }
+
+// tampilkanHaloDunia();
+function tampilkanAngka(int $jumlah, int $indeks = 1) {
+    echo "Perulangan ke-{$indeks} <br>";
+
+    //panggil diri sendiri selama $indeks <= $jumlah
+    if ($indeks < $jumlah) {
+        tampilkanAngka($jumlah, $indeks + 1);
+    }
+}
+
+tampilkanAngka(20);
+?>
