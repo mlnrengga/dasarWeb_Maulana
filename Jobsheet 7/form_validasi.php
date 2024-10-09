a7bb9392bc93c3ebf8d71f6df266d9007a27d44a
mlnrengga <mlnrengga@gmail.com>
2024-10-09 22:07:20 +0700

    add: soal 7.4

--- Jobsheet 7/form_validasi.php
+++ Jobsheet 7/form_validasi.php
@@ -14,6 +14,10 @@
         <label for="email">Email:</label>
         <input type="text" id="email" name="email">
         <span id="email-error" style="color: red;"></span><br>
+
+        <label for="password">password:</label>
+        <input type="password" id="password" name="password">
+        <span id="password-error" style="color: red;"></span><br>
         <input type="submit" value="Submit">
     </form>
 
@@ -25,6 +29,7 @@
                 event.preventDefault();                 
                 var nama = $("#nama").val();
                 var email = $("#email").val();
+                var password = $("#password").val();
                 var valid = true;
 
                 if (nama == "") {
@@ -39,6 +44,13 @@
                     valid = false;
                 } else {
                     $("#email-error").text("");
+                }
+
+                if(password.length < 8) {
+                    $("#password-error").text("Password minimal 8 karakter");
+                    valid = false;
+                } else {
+                    $("#password-error").text("");
                 }
 
                 if (valid) {
