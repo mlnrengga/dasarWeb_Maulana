7e56015e956e2fb42737a2ac606ace7a762ae3ca
mlnrengga <mlnrengga@gmail.com>
2024-10-18 17:45:18 +0700

    add: prak3_soal11

--- Jobsheet 8/upload_ajax.php
+++ Jobsheet 8/upload_ajax.php
@@ -1,27 +1,44 @@
 <?php 
-if (isset($_FILES['file'])) {
-    $errors = array();
-    $file_name = $_FILES['file']['name'];
-    $file_size = $_FILES['file']['size'];
-    $file_tmp = $_FILES['file']['tmp_name'];
-    $file_type = $_FILES['file']['type'];
-    @$file_ext = strtolower(end(explode('.', $_FILES['file']['name'])));
+$errors = [];
 
-    $extensions = array("pdf", "doc", "docx", "txt");
+if (isset($_FILES['files'])) {
+    $allowedExtensions = ['jpg', 'jpeg', 'png', 'gif'];
+    $maxsize = 2 * 1024 * 1024;
+    $targetDirectory = 'documents/';
 
-    if (!in_array($file_ext, $extensions)) {
-        $errors[] = "Ekstensi file yang diizinkan adalah PDF, DOC, DOCX, atau TXT.";
+    if (!file_exists($targetDirectory)) {
+        mkdir($targetDirectory, 0777, true);
     }
 
-    if ($file_size > 2097152) {
-        $errors[] = 'Ukuran file tidak boleh lebih dari 2 MB';
-    }
+    $totalFiles = count($_FILES['files']['name']);
+    for ($i = 0; $i < $totalFiles; $i++) {
+        $file_name = $_FILES['files']['name'][$i];
+        $file_size = $_FILES['files']['size'][$i];
+        $file_tmp = $_FILES['files']['tmp_name'][$i];
+        $file_type = $_FILES['files']['type'][$i];
+        $file_ext = strtolower(pathinfo($file_name, PATHINFO_EXTENSION));
 
-    if (empty($errors)) {
-        move_uploaded_file($file_tmp, "documents/" . $file_name);
-        echo "File berhasil diunggah.";
-    } else {
-        echo implode("", $errors);
+        if (!in_array($file_ext, $allowedExtensions)) {
+            $errors[] = "Ekstensi file $file_name tidak diizinkan. Hanya file dengan ekstensi JPG, JPEG, PNG, atau GIF yang diizinkan.";
+        }
+
+        if ($file_size > $maxsize) {
+            $errors[] = "Ukuran file $file_name tidak boleh lebih dari 2 MB.";
+        }
+
+        if (empty($errors)) {
+            $targetFile = $targetDirectory . basename($file_name);
+
+            if (move_uploaded_file($file_tmp, $targetFile)) {
+                echo "File $file_name berhasil diunggah.<br>";
+            } else {
+                echo "Gagal mengunggah file $file_name.<br>";
+            }
+        } else {
+            echo implode("", $errors) . "<br>";
+        }
     }
+} else {
+    echo "Tidak ada file yang diunggah.";
 }
 ?>
